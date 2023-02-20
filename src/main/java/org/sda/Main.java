7d495bab775760c39d6ce70cfd537e4701ff1e8e
Kätlin <kpadesaar@gmail.com>
2023-02-20 19:07:22 +0200

    Initial commit

---
+++ src/main/java/org/sda/Main.java
@@ -0,0 +1,12 @@
+package org.sda;
+
+/**
+ * @author Kätlin Padesaar-Korela
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
